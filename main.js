ece488f57659fd2715a31f0b97aefc624e172892
LN <leonelngoya@gmail.com>
2024-04-04 04:42:38 +0100

    first commit

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-import './style.scss'
-import gsap from 'gsap'
+import "./style.scss";
+
+const items = document.querySelectorAll(".item");
+
+items.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("active");
+    items.forEach((el, i) => {
+      if (i !== index) {
+        el.classList.toggle("inactive");
+      }
+    });
+  });
+});
